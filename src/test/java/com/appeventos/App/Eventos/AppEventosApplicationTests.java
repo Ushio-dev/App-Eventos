d0fe78b2664470a6fec0b3d250f11363d6442a8a
Franco <fran14korn@live.com>
2023-05-24 19:47:37 -0300

    Initial commit

---
+++ src/test/java/com/appeventos/App/Eventos/AppEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appeventos.App.Eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
